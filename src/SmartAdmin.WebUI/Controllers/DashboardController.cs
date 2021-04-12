afdcd9bcca5d73bf3df8df45307fa1f16ab83bf8
daenuli <giarsyani.nuli@gmail.com>
2021-04-12 16:12:33 +0700

    update dashboard

--- src/SmartAdmin.WebUI/Controllers/DashboardController.cs
+++ src/SmartAdmin.WebUI/Controllers/DashboardController.cs
@@ -18,22 +18,26 @@ namespace SmartAdmin.WebUI.Controllers
             var AllProject = _db.Project.Count();
 
             ViewBag.Active = _db.Project.Where(p => p.status == 22 || p.status == 30).Count();
-            ViewBag.Finish = _db.Project.Where(p => p.status == 20 || p.status == 23).Count();
+            ViewBag.Finish = _db.Project.Where(p => p.status == 23).Count();
+            ViewBag.FinishTagihan = _db.Project.Where(p => p.status == 31).Count();
 
-            ViewBag.NewProject = _db.Project.Where(p => p.status == 20 || p.status == 21).ToList();
+            ViewBag.NewProject = _db.Project.Where(p => p.status == 22 || p.status == 30).ToList();
+            ViewBag.ProgressTagihan = _db.Project
+                .Where(p => p.status == 22 || p.status == 23 || p.status == 30 || p.status == 31)
+                .ToList();
 
-            var TotalRKU = _db.Project.Where(p => p.sub_inisiasi_id == 1).Count();
-            var TotalRKAP = _db.Project.Where(p => p.sub_inisiasi_id == 2).Count();
-            var TotalMOC = _db.Project.Where(p => p.status == 23 || p.status == 31 || p.status == 20 || p.status == 21)
+            var TotalRKU = _db.Project.Where(p => p.status == 20 || p.status == 31)
+                            .Where(p => p.sub_inisiasi_id == 1).Count();
+            var TotalRKAP = _db.Project.Where(p => p.status == 20 || p.status == 31)
+                            .Where(p => p.sub_inisiasi_id == 2).Count();
+            var TotalMOC = _db.Project.Where(p => p.status == 20 || p.status == 31)
                         .Where(p => p.sub_inisiasi_id == 3).Count();
-            var TotalSU = _db.Project.Where(p => p.status == 23 || p.status == 31 || p.status == 20 || p.status == 21)
+            var TotalSU = _db.Project.Where(p => p.status == 20 || p.status == 31)
                         .Where(p => p.sub_inisiasi_id == 4).Count();
-            var TotalNonRKAP = _db.Project.Where(p => p.status == 23 || p.status == 31 || p.status == 20 || p.status == 21)
+            var TotalNonRKAP = _db.Project.Where(p => p.status == 20 || p.status == 31)
                         .Where(p => p.sub_inisiasi_id == 5).Count();
                         
             ViewBag.RKU = 100 * TotalRKU / AllProject;
-            // ViewBag.RKU = (decimal) (AllProject / AllProject);
-            // ViewBag.RKU = (TotalRKU/AllProject)*100;
             ViewBag.RKAP = 100 * TotalRKAP / AllProject;
             ViewBag.MOC = 100 * TotalMOC / AllProject;
             ViewBag.SU = 100 * TotalSU / AllProject;
